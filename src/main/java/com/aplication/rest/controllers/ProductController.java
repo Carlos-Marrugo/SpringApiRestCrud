af05a2e87d3699fa90eb29d7b91f52942694d195
Carlos <elrprogramadortutoriales@gmail.com>
2024-09-06 00:16:31 -0500

    Added save, update, and delete endpoints for ProductController

--- src/main/java/com/aplication/rest/controllers/ProductController.java
+++ src/main/java/com/aplication/rest/controllers/ProductController.java
@@ -1,14 +1,13 @@
 package com.aplication.rest.controllers;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.aplication.rest.controllers.dto.ProductDTO;
 import com.aplication.rest.entities.Product;
@@ -46,12 +45,58 @@ public class ProductController {
                 .map(product -> ProductDTO.builder()
                 .id(product.getId())
                 .name(product.getName())
-                .price(product.getPrice())
+                .precio(product.getPrecio())
                 .maker(product.getMaker())
                 .build()
                 )
                 .toList();
 
         return ResponseEntity.ok(productList);
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity<?> save(@RequestBody ProductDTO productDTO) throws URISyntaxException {
+
+        if (productDTO.getName().isBlank() || productDTO.getPrecio() == null || productDTO.getMaker() == null) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        Product product = Product.builder()
+                .name(productDTO.getName())
+                .precio(productDTO.getPrecio())
+                .maker(productDTO.getMaker())
+                .build();
+
+        productService.saveProduct(product);
+
+
+        return ResponseEntity.created(new URI("/api/product/save")).build();
+    }
+
+    public ResponseEntity<?> update(@PathVariable Long id, @RequestBody ProductDTO productDTO) {
+
+        Optional<Product> productOptional = productService.findById(id);
+
+        if(productOptional.isPresent()) {
+            Product product = productOptional.get();
+
+            product.setName(productDTO.getName());
+            product.setPrecio(productDTO.getPrecio());
+            product.setMaker(productDTO.getMaker());
+            productService.saveProduct(product);
+
+
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> daleteById(@PathVariable Long id) {
+        if(id != null) {
+            productService.deleteByid(id);
+            return ResponseEntity.ok("great Mother*****");
+        }
+
+        return ResponseEntity.badRequest().build();
     }
 }
